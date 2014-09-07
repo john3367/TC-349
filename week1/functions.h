e7c27c06baef8c9a918c1c7d53ead5cad10e5b70
Trisha <john3367@msu.edu>
2014-09-07 13:13:46 -0400

    updated

---
+++ week1/functions.h
@@ -0,0 +1,32 @@
+/* 
+ * File:   functions.h
+ * Author: gleaso83
+ *
+ * Created on February 25, 2014, 2:58 PM
+ */
+
+#ifndef FUNCTIONS_H
+#define	FUNCTIONS_H
+
+#include<string>
+#include<vector>
+
+#include <map>
+using std::map;
+using std::pair;
+using std::string;
+using std::vector;
+
+
+long collatz(long n);
+
+string pair_to_string(const pair<long, vector<long>> &p);
+
+void fill_vector(map<long, vector<long>> &m, vector<long> &v, long start);
+
+
+
+
+
+#endif	/* FUNCTIONS_H */
+
